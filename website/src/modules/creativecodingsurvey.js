10a1b541ea0ebadbcd86fa1a3d1656138f48c21f
Sietse van der Meer <sietsevander@immermeer.eu>
2021-12-29 23:42:34 +0100

    Introduce p5 canvas & configure its dimensions, listen for resize events

--- website/src/modules/creativecodingsurvey.js
+++ website/src/modules/creativecodingsurvey.js
@@ -1,8 +1,51 @@
 import { loadJSON } from '../utils/ajax.js';
 import { surveyData } from '../data/surveyData.js';
 
+import * as P5 from "p5";
+
 import '../css/creativecodingsurvey.scss';
 
+export class CreativeCodingSurvey {
+    constructor(element, options) {
+        this.starField = [];
+    }
+
+    setup(sketch, surveyData) {
+        this.sketch = sketch;
+        this.surveyData = surveyData;
+
+        sketch.createCanvas(sketch.windowWidth, sketch.windowHeight)
+        for(let i = 0; i < 80; i++){
+            const randX = sketch.round(sketch.random(0, window.innerWidth));
+            const randY = sketch.round(sketch.random(0, window.innerHeight));
+            const randR = sketch.random(2, 4);
+
+            // new makeStar(randX, randY, randR)
+            this.starField.push({
+                draw: () => {
+                    sketch.circle(randX, randY, randR)
+                    sketch.fill(255);
+                }}
+            );
+        }
+    }
+
+    draw(sketch) {
+        // console.log(this.surveyData)
+
+        sketch.background(0);
+
+        this.starField.map((starPoint) => {
+            starPoint.draw()
+        });
+    }
+
+    windowResized(sketch) {
+        sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+    }
+}
+
+const projectCanvas = new CreativeCodingSurvey();
 export default element => {
     console.log('Component mounted on', element);
 
@@ -11,4 +54,12 @@ export default element => {
     }, (error) => {
         console.log('stale local data', surveyData)
     });
+
+    const thisSketch = ( sketch ) => {
+        sketch.setup = () => projectCanvas.setup(sketch, surveyData);
+        sketch.draw = () => projectCanvas.draw(sketch);
+        sketch.windowResized = () => projectCanvas.windowResized(sketch);
+    };
+
+    let myp5 = new P5(thisSketch);
 };
