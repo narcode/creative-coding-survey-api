c427758ddd9ff263be99b70d928ba8f5bc5cdf35
Sietse van der Meer <sietsevander@immermeer.eu>
2021-12-30 21:28:57 +0100

    Add missing svg type icons, parse total & individual counts to navbar

--- website/src/modules/creativecodingsurvey.js
+++ website/src/modules/creativecodingsurvey.js
@@ -12,37 +12,57 @@ export class CreativeCodingSurvey {
     }
 
     preload(sketch) {
-        this.iconSet['enthusiast'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['maker'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['organisation'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['contributor'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['venue'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['event'] = sketch.loadImage('/img/enthusiasts.svg')
-        this.iconSet['anonymous'] = sketch.loadImage('/img/enthusiasts.svg')
+        this.iconSet['enthusiast']      = sketch.loadImage('/img/enthusiast.svg')
+        this.iconSet['maker']           = sketch.loadImage('/img/maker.svg')
+        this.iconSet['organisation']    = sketch.loadImage('/img/venue.svg')
+        this.iconSet['contributor']     = sketch.loadImage('/img/contributor.svg')
+        this.iconSet['venue']           = sketch.loadImage('/img/venue.svg')
+        this.iconSet['event']           = sketch.loadImage('/img/event.svg')
+        this.iconSet['anonymous']       = sketch.loadImage('/img/anonymous.svg')
     }
 
     setup(sketch, surveyData) {
         this.sketch             = sketch;
         this.surveyData         = surveyData;
+        this.typeCount          = {
+            enthusiast: 0,
+            maker: 0,
+            organisation: 0,
+            contributor: 0,
+            venue: 0,
+            event: 0,
+            anonymous: 0,
+        };
 
-        // resize icons
-        this.iconSet['enthusiast'].resize(15,15)
+        this.resizeIcons();
 
         sketch.createCanvas(sketch.windowWidth, sketch.windowHeight)
 
+        // create definitive set of results,
         this.surveyData.map((responseEntity) => {
-            const randX = sketch.round(sketch.random(0, window.innerWidth));
-            const randY = sketch.round(sketch.random(0, window.innerHeight));
+            const randX         = sketch.round(sketch.random(0, window.innerWidth));
+            const randY         = sketch.round(sketch.random(0, window.innerHeight));
+            const entityType    = responseEntity.responses.type.length ? responseEntity.responses.type[0].toString().toLowerCase().trim() : 'anonymous';
 
-            // new makeStar(randX, randY, randR)
+            this.typeCount[entityType]++;
+
+            // add an initial entity position, randomly relative to the current viewport
             this.canvasEntitites.push({
                 entity: responseEntity,
+                coordinates: { x: randX, y: randY },
+                state: 'inactive',
                 draw: () => {
-                    sketch.image(this.iconSet['enthusiast'], randX, randY);
+                    sketch.image(this.iconSet[entityType], randX, randY);
                 }}
             );
         });
-        console.log(this.canvasEntitites);
+        for (const [type, count] of Object.entries(this.typeCount)) {
+            const typeContainer = document.querySelector( `.menu li.${type} a`);
+            typeContainer.setAttribute('data-value', count);
+        }
+        const totalCountContainer = document.querySelector( `.menu li:first-of-type`);
+        totalCountContainer.setAttribute('data-value', this.surveyData.length);
+
     }
 
     draw(sketch) {
@@ -55,6 +75,19 @@ export class CreativeCodingSurvey {
 
     windowResized(sketch) {
         sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
+
+        /* todo: we should probably recalculate the entities positions here, relative to the new canvas dimensions */
+    }
+
+    // resize icons
+    resizeIcons() {
+        for (let icon in this.iconSet) {
+            this.iconSet[icon].resize(20,20);
+        }
+    }
+
+    plotEntities() {
+        console.log(this.canvasEntitites);
     }
 }
 
